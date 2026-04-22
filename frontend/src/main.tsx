1414a03feefa9d8252b4b5491d91ee6daa2be906
W3ndig0 <Jingxiang304@gmail.com>
2026-04-22 16:55:36 +0200

    add: toastify

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { ToastContainer } from "react-toastify";
+import App from "./App";
 import "./index.css";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ToastContainer position="top-right" autoClose={2000} />
+    </QueryClientProvider>
   </StrictMode>,
 );
