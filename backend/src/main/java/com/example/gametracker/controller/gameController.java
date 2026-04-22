15bb6b213d9874ad40cdfffc8e4c90ac8d7145e5
W3ndig0 <Jingxiang304@gmail.com>
2026-04-22 15:20:21 +0200

    add: cors in controller

--- backend/src/main/java/com/example/gametracker/controller/gameController.java
+++ backend/src/main/java/com/example/gametracker/controller/gameController.java
@@ -1,15 +1,16 @@
 package com.example.gametracker.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@CrossOrigin(origins = {"https://gamebacklog-tracker.pages.dev", "http://localhost:5173"})@RestController
 @RequestMapping()
 public class gameController {
     @GetMapping("/api/games")
-    
+
     public ResponseEntity<String> getGames() {
         return ResponseEntity.ok("Games");
     }
